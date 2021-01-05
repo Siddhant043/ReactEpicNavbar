7fc5e433ab2ee5cd763b7b7eefee97fcb08ba392
Siddhant Mishra <siddhant48311@gmail.com>
2021-01-05 17:51:51 +0530

    Navbar created with dropdown right shift glitch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import ContactUs from './components/contact-us/ContactUs';
+import Services from './components/services/Services';
+import Products from './components/products/Products';
+import SignUp from './components/signup/SignUp';
+import Consulting from './components/consulting/Consulting';
+import Marketing from './components/marketing/Marketing';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+      <Route path='/' exact component={Home}/>
+      <Route path='/sevices' exact component={Services}/>
+      <Route path='/products' exact component={Products}/>
+      <Route path='/contact-us' exact component={ContactUs}/>
+      <Route path='/sign-up' exact component={SignUp}/>
+      <Route path='/consulting' exact component={Consulting}/>
+      <Route path='/marketing' exact component={Marketing}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/marketing/Marketing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Marketing = () => {
+    return (
+        <div>
+        <h1 className='marketing'>MARKETING</h1>
+        </div>
+    )
+}
+
+export default Marketing
